3adbc905eceb3eefe61ff154d06298a4320ae2c3
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-22 23:57:09 +0500

    code is added

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const images = document.querySelectorAll('.images');
 
 let slideNumber = 1;
 right.addEventListener('click', ()=>{
+    if(slideNumber < images.length){
     // console.log("Hello");
     slider.style.transform = `translateX(-${slideNumber*600}px)`;
     slideNumber++;
+} else {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+}
+    
 })

32975006e8311da751169d0584190002cad261ed
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-23 04:56:20 +0500

    code is added

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ const images = document.querySelectorAll(".image");
 let slideNumber = 1;
 const length = images.length;
 
+
+const bottom = document.querySelector('.bottom')
+
+for(let i=0; i<length; i++){
+    const div = document.createElement('div');
+    div.className = "btn"
+    bottom.appendChild(div)
+}
+
+const buttons = document.querySelectorAll('.btn')
+buttons[0].style.backgroundColor = 'white';
+
+const resetBtn = () => {
+    buttons.forEach((button)=>{
+        button.style.backgroundColor = 'transparent';
+    });
+}
+
+buttons.forEach((button, i) => {
+    button.addEventListener('click', () =>{
+        resetBtn();
+        slider.style.transform = `translateX(-${i*600}px)`;
+        slideNumber = i + 1;
+        button.style.backgroundColor = 'white'
+    });
+});
+
+const changeColor = () => {
+    resetBtn();
+    buttons[slideNumber-1].style.backgroundColor = 'white';
+}
+
 const nextSlide = () => {
   slider.style.transform = `translateX(-${slideNumber * 600}px)`;
   slideNumber++;
@@ -26,28 +58,10 @@ const getLastSlide = () => {
 
 right.addEventListener("click", () => {
     slideNumber < length ? nextSlide() : getFirstSlide();
+    changeColor();
 });
 left.addEventListener("click", () => {
     slideNumber > 1 ? previousSlide() : getLastSlide();
+    changeColor();
 });
 
-const bottom = document.querySelector('.bottom')
-
-for(let i=0; i<length; i++){
-    const div = document.createElement('div');
-    div.className = "btn"
-    bottom.appendChild(div)
-}
-
-const buttons = document.querySelectorAll('.btn')
-buttons[0].style.backgroundColor = 'white';
-
-const resetBtn = () => {
-
-}
-
-buttons.forEach((button, i) => {
-    button.addEventListener('click', () =>{
-        slider.style.transform = `translateX(-${i*800}px)`;
-    })
-});

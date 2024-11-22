af9f5bd78eb488e5486c57332dee7873cf27b8e5
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-23 00:31:36 +0500

    code is added

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.images');
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
 
 let slideNumber = 1;
-right.addEventListener('click', ()=>{
-    if(slideNumber < images.length){
-    // console.log("Hello");
-    slider.style.transform = `translateX(-${slideNumber*600}px)`;
-    slideNumber++;
-} else {
-    slider.style.transform = `translateX(0px)`;
-    slideNumber = 1;
-}
+const length = images.length;
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 600}px)`;
+  slideNumber++;
+};
+
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+right.addEventListener("click", () => {
     
-})
+    slideNumber < length ? nextSlide() : getFirstSlide();
+});

ab12739cf2dd632c33020df87f5d008b753b4817
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-22 23:54:52 +0500

    code is added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.images');
 
 let slideNumber = 1;
 right.addEventListener('click', ()=>{
